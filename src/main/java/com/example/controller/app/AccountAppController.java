9d78430461d56483bbf9fbe7c5bea3569d90e106
ry0135 <namdpde170457@fpt.edu.vn>
2025-08-26 11:31:16 +0700

    comiit

---
+++ src/main/java/com/example/controller/app/AccountAppController.java
@@ -0,0 +1,81 @@
+package com.example.controller.app;
+
+import com.example.model.Account;
+import com.example.model.ApiMessage;
+import com.example.repository.AccountRepository;
+import com.example.service.AccountService;
+import com.example.service.EmailService;
+import com.example.service.RandomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController("accountControllerApp")
+@RequestMapping("/api/accounts")
+public class AccountAppController {
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private RandomService randomService;
+
+    @Autowired
+    private EmailService emailService;
+    @Autowired
+    private AccountRepository accountRepository;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody Account account) {
+
+        String encodedPassword = passwordEncoder.encode(account.getPassword());
+        String code = randomService.generateRandomCode();
+        if (accountService.isEmailExistsAndIsVerifiedTrue(account.getEmail())){
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(new ApiMessage("EMAIL_REGISTERED", "Email đã đăng ký"));
+        }if (accountService.isEmailExistsAndIsVerifiedFalse(account.getEmail())) {
+            Optional<Account> existOpt = accountRepository.findByEmail(account.getEmail());
+            if (existOpt.isPresent()) {
+                Account existing = existOpt.get();
+                existing.setCode(code);
+                existing.setPassword(account.getPassword());
+                existing.setFullName(account.getPassword());
+                existing.setPassword(account.getPassword());
+                // cập nhật code mới
+                accountRepository.save(existing);
+                emailService.sendCodeToEmail(existing.getEmail(), code);
+
+                return ResponseEntity.status(HttpStatus.OK)
+                        .body(new ApiMessage("EMAIL_NOT_VERIFIED", "Email đã đăng ký nhưng chưa xác minh. Mã xác minh mới đã được gửi."));
+            }
+        }
+        account.setPassword(encodedPassword);
+        account.setCode(code);
+        account.setVerified(false);
+        accountRepository.save(account);
+        emailService.sendCodeToEmail(account.getEmail(), code);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ApiMessage("CREATED", "Đăng ký thành công. Vui lòng kiểm tra email để xác minh."));
+    }
+
+    @PostMapping("/verificode")
+    public ResponseEntity<?> verifyCode(@RequestBody Account account) {
+        boolean codeVerified = accountService.verifyCodeAndUpdateStatus(account.getEmail(), account.getCode());
+
+        if (codeVerified) {
+            account.setVerified(true);
+            return ResponseEntity.ok("Xác thực thành công!");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Mã xác minh không hợp lệ");
+        }
+    }
+
+}
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,55 @@
 package com.example.service;
 
+
+import com.example.model.Account;
+import com.example.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class AccountService {
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private EmailService emailService;
+    @Autowired
+    private RandomService randomService;
+
+
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    @Transactional
+    public void save(Account account) {
+
+        String code = randomService.generateRandomCode();
+        String encodedPassword = passwordEncoder.encode(account.getPassword());
+        account.setPassword(encodedPassword);
+        account.setCode(code);
+
+        accountRepository.save(account);
+        emailService.sendCodeToEmail(account.getEmail(), code);
+    }
+    @Transactional(readOnly = true)
+    public boolean isEmailExistsAndIsVerifiedTrue (String email) {
+        return accountRepository.existsByEmailAndIsVerifiedTrue(email);
+    }
+
+    @Transactional(readOnly = true)
+    public boolean isEmailExistsAndIsVerifiedFalse (String email) {
+        return accountRepository.existsByEmailAndIsVerifiedFalse(email);
+    }
+
+    @Transactional
+    public boolean verifyCodeAndUpdateStatus(String email, String code) {
+        Account account = accountRepository.findByEmailAndCode(email, code);
+        if (account != null) {
+            // Cập nhật trạng thái tài khoản
+            account.setStatus(1);
+            accountRepository.save(account);
+            return true;
+        }
+        return false;
+    }
 }
